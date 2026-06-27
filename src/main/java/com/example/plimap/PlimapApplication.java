2fd4dc31ba034c172b8feea3293a053047a336a9
joobogyeong <162878426+joobogyeong@users.noreply.github.com>
2026-06-27 14:48:57 +0900

    init

---
+++ src/main/java/com/example/plimap/PlimapApplication.java
@@ -0,0 +1,13 @@
+package com.example.plimap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlimapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlimapApplication.class, args);
+    }
+
+}
