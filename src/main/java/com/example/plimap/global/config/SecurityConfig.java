1861210dfa53a2f43a8b841992e8f43c81ca841c
yerimi00 <fhddl1019@naver.com>
2026-07-11 15:40:37 +0900

    feat : 임시 스웨거 테스트용 토큰 발급 api 추가

--- src/main/java/com/example/plimap/global/config/SecurityConfig.java
+++ src/main/java/com/example/plimap/global/config/SecurityConfig.java
@@ -35,7 +35,8 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/oauth/**",
                                 "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/api/v1/auth/token/test"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
