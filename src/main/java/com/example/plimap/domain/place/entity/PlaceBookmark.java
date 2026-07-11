044411502b9451314a866004aaa19d926cafe07c
주보경 <powerbodol01@gmail.com>
2026-07-11 14:40:18 +0900

    feat: Place 도메인 연관 엔티티 구현

---
+++ src/main/java/com/example/plimap/domain/place/entity/PlaceBookmark.java
@@ -0,0 +1,32 @@
+package com.example.plimap.domain.place.entity;
+
+import com.example.plimap.global.entity.BaseEntity;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "place_bookmark")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PlaceBookmark extends BaseEntity {
+
+    @EmbeddedId
+    private PlaceBookmarkId id;
+
+    @MapsId("placeId")
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(
+            name = "place_id",
+            nullable = false,
+            foreignKey = @ForeignKey(name = "fk_place_bookmark_place"))
+    private Place place;
+}
