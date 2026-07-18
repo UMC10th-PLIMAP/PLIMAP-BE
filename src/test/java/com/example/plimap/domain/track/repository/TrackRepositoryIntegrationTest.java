663e2d1644d3592e38d1c7fad29eac47dbc309f6
ywkim1m <ywkim1m@gmail.com>
2026-07-18 16:58:04 +0900

    refactor: builder 코드 스타일 리뷰 반영

--- src/test/java/com/example/plimap/domain/track/repository/TrackRepositoryIntegrationTest.java
+++ src/test/java/com/example/plimap/domain/track/repository/TrackRepositoryIntegrationTest.java
@@ -54,7 +54,10 @@ class TrackRepositoryIntegrationTest {
     void 활성_PlaceTrack을_장소와_트랙으로_조회한다() {
         Place place = savePlace();
         Track track = trackRepository.save(track("active-youtube-video-id"));
-        PlaceTrack savedPlaceTrack = placeTrackRepository.saveAndFlush(PlaceTrack.create(place, track));
+        PlaceTrack savedPlaceTrack = placeTrackRepository.saveAndFlush(PlaceTrack.builder()
+                .place(place)
+                .track(track)
+                .build());
 
         PlaceTrack foundPlaceTrack = placeTrackRepository
                 .findByPlace_IdAndTrack_IdAndDeletedAtIsNull(place.getId(), track.getId())
@@ -68,7 +71,10 @@ class TrackRepositoryIntegrationTest {
     void 삭제된_PlaceTrack을_조회하고_복구하면_활성_조회가_가능하다() {
         Place place = savePlace();
         Track track = trackRepository.save(track("deleted-youtube-video-id"));
-        PlaceTrack placeTrack = placeTrackRepository.saveAndFlush(PlaceTrack.create(place, track));
+        PlaceTrack placeTrack = placeTrackRepository.saveAndFlush(PlaceTrack.builder()
+                .place(place)
+                .track(track)
+                .build());
 
         placeTrack.delete();
         entityManager.flush();
@@ -94,16 +100,16 @@ class TrackRepositoryIntegrationTest {
     }
 
     private Track track(String providerTrackId) {
-        return Track.create(
-                "YOUTUBE",
-                providerTrackId,
-                "title",
-                "artist",
-                "album",
-                "https://example.com/album.jpg",
-                "https://example.com/preview",
-                180_000
-        );
+        return Track.builder()
+                .provider("YOUTUBE")
+                .providerTrackId(providerTrackId)
+                .title("title")
+                .artistName("artist")
+                .albumTitle("album")
+                .albumImageUrl("https://example.com/album.jpg")
+                .previewUrl("https://example.com/preview")
+                .durationMs(180_000)
+                .build();
     }
 
     private Place savePlace() {
