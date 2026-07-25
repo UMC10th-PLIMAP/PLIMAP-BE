339b8d19e5abc1542e065efb8ca5b80f77b3bcb1
seoyoon lee <leeseoyoon01@gmail.com>
2026-07-25 09:19:11 +0900

    fix: pl.id를 place_id로 수정

--- src/main/java/com/example/plimap/domain/pin/repository/query/impl/PinQueryRepositoryImpl.java
+++ src/main/java/com/example/plimap/domain/pin/repository/query/impl/PinQueryRepositoryImpl.java
@@ -51,7 +51,7 @@ public class PinQueryRepositoryImpl implements PinQueryRepository {
                        COUNT(*) AS pin_count
                 FROM pin
                 WHERE deleted_at IS NULL
-                        AND pl.id IN (:placeIds) 
+                        AND place_id IN (:placeIds) 
                 GROUP BY place_id
             ) pc
                 ON pc.place_id = pl.id
